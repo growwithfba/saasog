c0fc07fc79f233a022b7f95a7d9348a39495949a
Dave Keefe <your.email@example.com>
2025-04-27 21:21:38 -0500

    Supabase update middleware 2

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -7,17 +7,23 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
+        get(name) {
+          return cookieStore.get(name)?.value;
         },
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
+        set(name, value, options) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+            cookieStore.set(name, value, options);
           } catch {
-            // The `setAll` method was called from a Server Component.
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name, options) {
+          try {
+            cookieStore.set(name, '', options);
+          } catch {
+            // The `remove` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
